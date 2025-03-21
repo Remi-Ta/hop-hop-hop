ec1ddb4db9dd6e67a6f4017deaa8aa17b0009dbc
Remi-Ta <remitallon2@gmail.com>
2025-03-21 22:19:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const map = L.map('map').setView([48.8566, 2.3522], 13);
+    const map = L.map('map-container').setView([48.8566, 2.3522], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
